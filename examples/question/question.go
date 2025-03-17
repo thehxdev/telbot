e9a36e4f5a2d16f3f5c823c82f6c19301acf4a60
thehxdev <hossein.khosravi.ce@gmail.com>
2025-03-17 20:15:14 +0330

    question bot example based on conversation extention
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ examples/question/question.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/thehxdev/telbot"
+)
+
+const BOT_TOKEN string = "your_awesome_bot_token"
+
+func main() {
+	bot, err := telbot.New(BOT_TOKEN, "api.telegram.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conv, err := telbot.NewConversation(telbot.ConversationConfig{
+		Bot: bot,
+		Stages: []telbot.UpdateHandlerFunc{startHandler, nameHandler},
+	})
+
+	updatesChan, err := bot.StartPolling(telbot.UpdateParams{
+		Offset:  0,
+		Limit:   100,
+		Timeout: 30,
+		AllowedUpdates: []string{"message"},
+	})
+
+	log.Println("started polling updates...")
+	for update := range updatesChan {
+		if update.Message.Text == "/start" {
+			conv.Start(update)
+		}
+	}
+}
+
+func startHandler(bot *telbot.Bot, update telbot.Update) error {
+	_, err := bot.SendMessage(telbot.TextMessageParams{
+		ChatId: update.Message.Chat.Id,
+		Text: "Hey! This is a question bot. What is your name?",
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func nameHandler(bot *telbot.Bot, update telbot.Update) error {
+	_, err := bot.SendMessage(telbot.TextMessageParams{
+		ChatId: update.Message.Chat.Id,
+		Text: fmt.Sprintf("Nice to meet you %s!", update.Message.Text),
+	})
+	if err != nil {
+		return err
+	}
+	return &telbot.ErrConversationEnd{}
+}
