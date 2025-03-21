c9b000f4fab508bc749fdfd4af6dfe8bb6f42c41
thehxdev <hossein.khosravi.ce@gmail.com>
2025-03-21 07:24:41 +0330

    removed old directory structure files
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- user.go
+++
@@ -1,9 +0,0 @@
-package telbot
-
-type User struct {
-	Id        int    `json:"id"`
-	IsBot     bool   `json:"is_bot"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name,omitempty"`
-	Username  string `json:"username,omitempty"`
-}
